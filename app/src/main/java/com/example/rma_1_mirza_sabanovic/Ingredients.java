9151d59a45d1d81f28181ca4badacb30a716d354
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-04-29 13:40:25 +0200

    getting value of hint where is entered a value

--- app/src/main/java/com/example/rma_1_mirza_sabanovic/Ingredients.java
+++ app/src/main/java/com/example/rma_1_mirza_sabanovic/Ingredients.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Ingredients extends AppCompatActivity {
 
@@ -18,32 +15,35 @@ public class Ingredients extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ingredients);
 
         button = findViewById(R.id.button3);
+
+        EditText editText1 = findViewById(R.id.editTextNumberDecimal);
+        EditText editText2 = findViewById(R.id.editTextNumberDecimal2);
+
+        String hintEditText1 = getHintText(editText1);
+        String hintEditText2 = getHintText(editText2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //Dohvatanje vrijednosti iz ChooseSoapType
-                String chosenSoapType = getIntent().getStringExtra("chosenSoapType");
-                String chosenSoapWeight = getIntent().getStringExtra("chosenSoapWeight");
-                String chosenSuperfattingLevel = getIntent().getStringExtra("chosenSuperfattingLevel");
+                String arganOil = editText1.getText().toString();
+                String neemOil = editText2.getText().toString();
 
                 Intent intent = new Intent(Ingredients.this, Result.class);
-                intent.putExtra("chosenSoapType", chosenSoapType);
-                intent.putExtra("chosenSoapWeight", chosenSoapWeight);
-                intent.putExtra("chosenSuperfattingLevel", chosenSuperfattingLevel);
+                intent.putExtra("arganOil", arganOil);
+                intent.putExtra("neemOil", neemOil);
+                intent.putExtra("hintEditText1", hintEditText1);
+                intent.putExtra("hintEditText2", hintEditText2);
                 startActivity(intent);
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    private String getHintText(EditText editText) {
+        return editText.getHint().toString();
     }
 }
--- app/src/main/java/com/example/rma_1_mirza_sabanovic/Result.java
+++ app/src/main/java/com/example/rma_1_mirza_sabanovic/Result.java
@@ -1,35 +1,46 @@
 package com.example.rma_1_mirza_sabanovic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Result extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
+
+        btn = findViewById(R.id.button4);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Result.this, ChooseSoapType.class));
+            }
+        });
 
         String chosenSoapType = getIntent().getStringExtra("chosenSoapType");
         String chosenSoapWeight = getIntent().getStringExtra("chosenSoapWeight");
         String chosenSuperfattingLevel = getIntent().getStringExtra("chosenSuperfattingLevel");
+        String hintEditText1 = getIntent().getStringExtra("hintEditText1");
+        String hintEditText2 = getIntent().getStringExtra("hintEditText2");
+        String arganOil = getIntent().getStringExtra("arganOil");
+        String neemOil = getIntent().getStringExtra("neemOil");
 
         TextView textView6 = findViewById(R.id.textView6);
         textView6.setText("A " + chosenSoapType + " soap, measured in " + chosenSoapWeight + " with superfatting level of " + chosenSuperfattingLevel);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TextView textView19 = findViewById(R.id.textView19);
+        TextView textView20 = findViewById(R.id.textView20);
+
+        textView19.setText(arganOil.isEmpty() ? hintEditText1 : arganOil);
+        textView20.setText(neemOil.isEmpty() ? hintEditText2 : neemOil);
     }
-
-
 }
